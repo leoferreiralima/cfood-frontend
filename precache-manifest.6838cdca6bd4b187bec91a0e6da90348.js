7540ff0a3754ccd7115d9735c38712f0da3c7a90
leoferreiralima <leoferreiralima@yahoo.com>
2020-01-01 13:05:54 -0300

    Updates

---
+++ precache-manifest.6838cdca6bd4b187bec91a0e6da90348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff5eebe24b86c25e27107267a8c26b8",
+    "url": "/cfood-frontend/index.html"
+  },
+  {
+    "revision": "c3ce8bf4992c09605bb0",
+    "url": "/cfood-frontend/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c3ce8bf4992c09605bb0",
+    "url": "/cfood-frontend/static/js/2.dd7c4d38.chunk.js"
+  },
+  {
+    "revision": "4a2b392beb8f6afcfebceae89cc67a1d",
+    "url": "/cfood-frontend/static/js/2.dd7c4d38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfbeaf9d91971cd3d49a",
+    "url": "/cfood-frontend/static/js/main.6812780e.chunk.js"
+  },
+  {
+    "revision": "643723d3cd9d16d21a7e",
+    "url": "/cfood-frontend/static/js/runtime-main.3c92f8a9.js"
+  },
+  {
+    "revision": "7b70314ee979bd456ce565767ba88429",
+    "url": "/cfood-frontend/static/media/background.7b70314e.svg"
+  }
+]);
